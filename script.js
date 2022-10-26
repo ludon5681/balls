29d7b299577614981aaf881bddd26f15a19663d1
ludon5681 <lucadonais@gmail.com>
2022-10-26 14:21:51 -0400

    Added lots of user customizability

--- script.js
+++ script.js
@@ -1,23 +1,48 @@
 window.onload = function() {
-    Crafty.init(400, 400, document.getElementById("stage"));
-    // A blue block, controlled by arrow keys
+    document.addEventListener("keydown", (e) => {
+        keyName = e.key
+        if (keyName === "Enter" && document.getElementById("num").value !== "") {
+            e.preventDefault()
+            if ($("#num").val() < 10001) {
+                ball_count = $("#num").val()
+                $("#num").val("")
+                $("#num").removeClass("ballInputRed")
+                $("#num").attr("placeholder", "Default: 1000")
+            } else {
+                $("#num").val("")
+                $("#num").addClass("ballInputRed")
+                $("#num").attr("placeholder", "Max: 10 000")
+            }
+        }
+    })
 
+    var ball_count = 1000
+    var length = 5
+    var color = "blue"
+
+    // initialize the crafty canvas on the element with the id of "stage" and give it a width and height of 400px
+    Crafty.init(400, 400, document.getElementById("stage"));
+
+    // load a spritesheet (in this case just the one image of the dvd logo)
     var assetsObj = {
         "sprites": {
+            // refer to the path of the image
             "Assets/DVD_logoSmall.png": {
-                // This is the width of each image in pixels
+                // the width of one tile (in this case the width of the whole image)
                 tile: 50,
-                // The height of each image
+                // the height of one tile (in this case the height of the whole image)
                 tileh: 25,
-                // We give names to three individual images
+                // a map representing the top left corner of each sprite in the spritesheet (in this case just 0, 0 since it's just one image)
                 map: {
                     dvd_image: [0, 0],
                 }
             }
         }
     };
+    // tell crafty to load the spritesheet
     Crafty.load(assetsObj, () => {})
 
+    // make the border elements
     var floor = Crafty.e("2D, Canvas, Color, platform")
         .attr({x: 0, y: 390, w: 400, h: 10})
         .color("gray")
@@ -31,24 +56,62 @@ window.onload = function() {
         .attr({x: 0, y: 0, w: 400, h: 10})
         .color("gray")
 
-    document.getElementById("standard").addEventListener("click", function() {
-        Crafty("square").destroy()
+    // clear the screen when the user presses the clear button
+    document.getElementById("clear").addEventListener("click", () => { clear() })
 
+    // update the number of balls to bounce when the input is submitted
+    document.getElementById("ballsSubmit").addEventListener("click", function(e) {
+        e.preventDefault()
+        if ($("#num").val() < 10001) {
+            ball_count = $("#num").val()
+            $("#num").val("")
+        } else {
+            $("#num").val("")
+            $("#num").addClass("ballInputRed")
+            $("#num").attr("placeholder", "Max: 10 000")
+        }
+    })
+
+    $("#verySmall").on("click", () => { length = 1 })
+    $("#Small").on("click", () => { length = 5 })
+    $("#Medium").on("click", () => { length = 10 })
+    $("#Large").on("click", () => { length = 20 })
+    $("#veryLarge").on("click", () => { length = 35 })
+
+    $("#colorSubmit").on("click", () => { color = $("#color").val() })
+
+    function clear() {
+        // remove all elements on the screen with the "square" property
+        Crafty("square").destroy()
+    }
+
+    // when the user clicks the "standard" button
+    document.getElementById("standard").addEventListener("click", function() {
+        clear()
+        console.log(length)
+
+        // loop 1000 times
         var i = 0
-        while (i < 1000) {
+        while (i < ball_count) {
+            // create a crafty element with the variable name square[i]
             window["square" + i] = Crafty.e("2D, Canvas, Color, Gravity, square")
-                .attr({x: 200, y: 100, w: 5, h: 5, vy: 100})
-                .color("blue")
+                // define starting position and size, make it start with a downwards velocity of 100
+                .attr({x: 200 - length / 2, y: 100, w: length, h: length, vy: 100})
+                .color(color)
+                // run this function every frame
                 .bind("UpdateFrame", function() {
-                    var neg = Math.floor(Math.random() * 2)
+                    // if the ball hits the floor element's y position
                     if (this.y >= floor.y - this.h) {
+                        // assign a random y velocity that must be moving the opposite direction
                         this.vy = -(Math.random() * 200)
+                        // assign a random x velocity
                         if (Math.random() <= 0.5) {
                             this.vx = -(Math.random() * 200)
                         } else {
                             this.vx = Math.random() * 200
                         }
-                    } if (this.y <= ceiling.y + this.h) {
+                    // repeat
+                    } if (this.y <= ceiling.y + 10) {
                         this.vy = Math.random() * 200
                         if (Math.random() <= 0.5) {
                             this.vx = -(Math.random() * 200)
@@ -62,7 +125,7 @@ window.onload = function() {
                         } else {
                             this.vy = Math.random() * 200
                         }
-                    } if (this.x <= wall1.x + this.w) {
+                    } if (this.x <= wall1.x + 10) {
                         this.vx = Math.random() * 200
                         if (Math.random() <= 0.5) {
                             this.vy = -(Math.random() * 200)
@@ -75,10 +138,15 @@ window.onload = function() {
         }
     })
 
+    // when the user clicks the "screensaver" button
     document.getElementById("screensaver").addEventListener("click", function() {
-        Crafty("square").destroy()
+        clear()
+        
+        // create a crafty element that uses the "dvd_image" sprite map
         var dvd_logo = Crafty.e("2D, Canvas, Gravity, square, dvd_image")
+            // set starting position, size, and velocity
             .attr({x: 10, y: 10, w: 100, h: 50, vx: 70, vy: 30})
+            // every frame, if it goes out of bounds, reverse its x or y velocity
             .bind("UpdateFrame", function() {
                 if (this.x >= wall2.x - this.w | this.x <= wall1.x + 10) {
                     this.vx = -(this.vx)
@@ -87,4 +155,7 @@ window.onload = function() {
                 }
             })
     })
+
+    
+    
 };
